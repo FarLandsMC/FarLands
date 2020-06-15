551983ea8bdac63aacbc0b40fff5697e77faed64
Ian Smith <isilversmith810@gmail.com>
2020-06-15 17:18:31 -0400

    Started work on GameBase class

---
+++ src/net/farlands/sanctuary/mechanic/region/GameBase.java
@@ -0,0 +1,58 @@
+package net.farlands.sanctuary.mechanic.region;
+
+import com.kicas.rp.data.Region;
+import net.farlands.sanctuary.mechanic.Mechanic;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class GameBase<T> extends Mechanic {
+    protected final Map<Player, T> playerData;
+    protected final Region gameRegion;
+
+    protected GameBase(Region gameRegion) {
+        this.playerData = new HashMap<>();
+        this.gameRegion = gameRegion;
+    }
+
+    protected void onPlayerExitGame(Player player) { }
+
+    protected void onPlayerEnterGameRegion(Player player) { }
+
+    protected void onPlayerExitGameRegion(Player player) { }
+
+    protected void addPlayer(Player player, T data) {
+        playerData.put(player, data);
+    }
+
+    protected T getData(Player player) {
+        return playerData.get(player);
+    }
+
+    protected void removePlayer(Player player) {
+        playerData.remove(player);
+    }
+
+    @Override
+    public void onPlayerQuit(Player player) {
+        onPlayerExitGame(player);
+        playerData.remove(player);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerMove(PlayerMoveEvent event) {
+        if (gameRegion.contains(event.getFrom()) && !gameRegion.contains(event.getTo()))
+            onPlayerExitGameRegion(event.getPlayer());
+        else if (!gameRegion.contains(event.getFrom()) && gameRegion.contains(event.getTo()))
+            onPlayerEnterGameRegion(event.getPlayer());
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerTeleport(PlayerTeleportEvent event) {
+        onPlayerMove(event);
+    }
+}
