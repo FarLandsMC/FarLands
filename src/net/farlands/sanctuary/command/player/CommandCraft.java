0f8ee820d675af572c38ff96d95a301e1623342d
Ian Smith <isilversmith810@gmail.com>
2020-07-29 15:17:57 -0400

    Optimization of /craft

--- src/net/farlands/sanctuary/command/player/CommandCraft.java
+++ src/net/farlands/sanctuary/command/player/CommandCraft.java
@@ -2,14 +2,14 @@ package net.farlands.sanctuary.command.player;
 
 import static com.kicas.rp.util.TextUtils.sendFormatted;
 import com.kicas.rp.command.TabCompleterBase;
-import com.kicas.rp.util.Materials;
 import com.kicas.rp.util.Utils;
 
+import net.farlands.sanctuary.FarLands;
 import net.farlands.sanctuary.command.Category;
 import net.farlands.sanctuary.command.PlayerCommand;
 import net.farlands.sanctuary.data.Rank;
 
-import net.farlands.sanctuary.util.FLUtils;
+import net.farlands.sanctuary.util.Pair;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -19,11 +19,26 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.*;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class CommandCraft extends PlayerCommand {
-    private static final List<String> CRAFTABLE_MATERIALS = Arrays.stream(Material.values())
-            .filter(Materials::hasRecipe).map(Utils::formattedName).collect(Collectors.toList());
+    private static final Set<String> CRAFTABLE_MATERIALS = new HashSet<>();
+    private static final HashMap<Material, List<Recipe>> RECIPES = new HashMap<>();
+
+    static {
+        Iterator<Recipe> itr = Bukkit.recipeIterator();
+        while (itr.hasNext()) {
+            Recipe recipe = itr.next();
+            Material resultType = recipe.getResult().getType();
+            CRAFTABLE_MATERIALS.add(Utils.formattedName(resultType));
+            List<Recipe> recipes = RECIPES.get(resultType);
+            if (recipes == null) {
+                recipes = new ArrayList<>();
+                recipes.add(recipe);
+                RECIPES.put(resultType, recipes);
+            } else
+                recipes.add(recipe);
+        }
+    }
 
     public CommandCraft() {
         super(Rank.PATRON, Category.UTILITY, "Open a crafting window or craft a specific item. Note: you will need " +
@@ -61,18 +76,19 @@ public class CommandCraft extends PlayerCommand {
             }
         }
 
-        Inventory inventory = sender.getInventory();
+        long startTime = System.nanoTime();
+        Cache cache = new Cache(sender);
 
         // Try to craft the requested amount
         int crafted = 0;
         while (crafted < amount) {
-            int additional = takeRequirements(sender, inventory, new Stack<>(), item, true);
+            int additional = takeRequirements(cache, item, true);
             if (additional == 0)
                 break;
             crafted += additional;
         }
 
-        give(item, crafted, sender, inventory, true);
+        cache.give(item, crafted, true);
         sender.updateInventory();
 
         if (crafted == 0)
@@ -80,6 +96,8 @@ public class CommandCraft extends PlayerCommand {
         else if (crafted < amount)
             sender.sendMessage(ChatColor.RED + "You only had enough resources to craft " + crafted + " of this item.");
 
+        long elapsed = System.nanoTime() - startTime;
+        FarLands.getDebugger().echo("/craft " + String.join(" ", args) + " - Time: " + ((elapsed / 1000) / 1000.0) + "ms");
         return true;
     }
 
@@ -95,31 +113,38 @@ public class CommandCraft extends PlayerCommand {
         }
     }
 
-    private void give(Material material, int amount, Player recipient, Inventory inventory, boolean sendMessages) {
-        while (amount > 0) {
-            int stackSize = Math.min(amount, material.getMaxStackSize());
-            FLUtils.giveItem(recipient, inventory, recipient.getLocation(), new ItemStack(material, stackSize), sendMessages);
-            amount -= stackSize;
-        }
-    }
-
-    private int takeRequirements(Player sender, Inventory inv, Stack<Material> materialStack, Material material, boolean sendMessages) {
+    private int takeRequirements(Cache cache, Material material, boolean sendMessages) {
         // Prevent recursion for items like iron ingots and wheat
-        if (materialStack.contains(material))
+        if (cache.materialStack.contains(material))
             return 0;
-        materialStack.push(material);
+        cache.pushStack(material);
 
         // Try to find recipes for the item
-        List<Recipe> recipes = Bukkit.getRecipesFor(new ItemStack(material, 1));
-        if (recipes.isEmpty()) {
+        List<Recipe> recipes = RECIPES.get(material);
+        if (recipes == null) {
             if (sendMessages)
-                sendFormatted(sender, "&(red)This item does not have a recipe.");
-            materialStack.pop();
+                sendFormatted(cache.player, "&(red)This item does not have a recipe.");
+            cache.popStack();
             return 0;
         }
 
-        // Copy the contents so we can restore if the take fails
-        ItemStack[] originalContents = clonedContents(inv);
+        // Try to use a cached recipe
+        Pair<List<Material>, Integer> cachedRecipe = cache.cachedRecipes.get(material);
+        if (cachedRecipe != null) {
+            boolean fulfilledAllRequirements = true;
+            for (Material requirement : cachedRecipe.getFirst()) {
+                if (!takeOne(cache, requirement)) {
+                    fulfilledAllRequirements = false;
+                    break;
+                }
+            }
+
+            if (fulfilledAllRequirements) {
+                cache.popStack();
+                return cachedRecipe.getSecond();
+            } else
+                cache.restoreInventory();
+        }
 
         // Look for a recipe variant that we have the necessary items for
         recipeIter:
@@ -133,55 +158,197 @@ public class CommandCraft extends PlayerCommand {
                     : ((ShapelessRecipe) recipe).getChoiceList();
 
             // Take items for each choice
+            List<Material> usedChoices = new ArrayList<>(9);
             requirementIter:
             for (RecipeChoice requirement : requirements) {
                 if (requirement == null)
                     continue;
 
                 for (Material choice : ((RecipeChoice.MaterialChoice) requirement).getChoices()) {
-                    if (takeOne(sender, inv, materialStack, choice))
+                    if (takeOne(cache, choice)) {
+                        usedChoices.add(choice);
                         continue requirementIter;
+                    }
                 }
 
                 // We could not find an item choice that we could satisfy
-                inv.setContents(originalContents);
+                usedChoices.clear();
+                cache.restoreInventory();
                 continue recipeIter;
             }
 
             // We have fulfilled every requirement
-            materialStack.pop();
+            cache.popStack();
+            cache.cachedRecipes.put(material, new Pair<>(usedChoices, recipe.getResult().getAmount()));
             return recipe.getResult().getAmount();
         }
 
         // We could not find a recipe where we could fulfill every requirement
-        materialStack.pop();
+        cache.popStack();
         return 0;
     }
 
-    private boolean takeOne(Player sender, Inventory inv, Stack<Material> materialStack, Material material) {
-        int first = inv.first(material);
-        if (first > -1) {
-            ItemStack stack = inv.getItem(first);
+    private boolean takeOne(Cache cache, Material material) {
+        Pair<ItemStack, Integer> slot = cache.nextMatching(material);
+        int index = slot.getSecond();
+        if (index > -1) {
+            cache.storeUndo(material, 1);
+
+            ItemStack stack = slot.getFirst();
             stack.setAmount(stack.getAmount() - 1);
 
-            if (stack.getAmount() == 0)
-                inv.remove(stack);
+            if (stack.getAmount() <= 0)
+                cache.inventory.remove(stack);
 
             return true;
         }
 
-        int crafted = takeRequirements(sender, inv, materialStack, material, false);
-        give(material, crafted - 1, sender, inv, false);
+        int crafted = takeRequirements(cache, material, false);
+        cache.give(material, crafted - 1, false);
         return crafted > 0;
     }
 
-    private static ItemStack[] clonedContents(Inventory inv) {
-        ItemStack[] contents = inv.getContents();
-        ItemStack[] clone = new ItemStack[contents.length];
-        for (int i = 0;i < contents.length;++ i) {
-            ItemStack original = contents[i];
-            clone[i] = original == null ? null : original.clone();
+    private static class Cache {
+        Player player;
+        Inventory inventory;
+        Stack<Material> materialStack;
+        Stack<HashMap<Material, Integer>> changeStack;
+        HashMap<Material, Integer> cachedSlots;
+        HashMap<Material, Pair<List<Material>, Integer>> cachedRecipes;
+
+        Cache(Player player) {
+            this.player = player;
+            this.inventory = player.getInventory();
+            this.materialStack = new Stack<>();
+            this.changeStack = new Stack<>();
+            this.cachedSlots = new HashMap<>();
+            this.cachedRecipes = new HashMap<>();
         }
-        return clone;
+
+        void storeUndo(Material material, int amount) {
+            if (!changeStack.isEmpty()) {
+                HashMap<Material, Integer> changes = changeStack.peek();
+                changes.put(material, changes.getOrDefault(material, 0) + amount);
+            }
+        }
+
+        void restoreInventory() {
+            HashMap<Material, Integer> changes = changeStack.peek();
+            for (Map.Entry<Material, Integer> undo : changes.entrySet()) {
+                Material material = undo.getKey();
+                int amount = undo.getValue();
+                if (amount > 0)
+                    give(undo.getKey(), amount, false);
+                else {
+                    int index;
+                    do {
+                        Pair<ItemStack, Integer> slot = nextMatching(material);
+                        index = slot.getSecond();
+                        ItemStack stack = slot.getFirst();
+                        int change = Math.max(amount, -stack.getAmount());
+                        stack.setAmount(stack.getAmount() + change);
+                        amount -= change;
+
+                        if (stack.getAmount() <= 0)
+                            inventory.remove(stack);
+                    } while (index > -1 && amount < 0);
+                }
+            }
+
+            changes.clear();
+        }
+
+        void pushStack(Material material) {
+            materialStack.push(material);
+            changeStack.push(new HashMap<>());
+        }
+
+        void popStack() {
+            materialStack.pop();
+            HashMap<Material, Integer> newReversions = changeStack.pop();
+            if (!changeStack.isEmpty()) {
+                HashMap<Material, Integer> reversions = changeStack.peek();
+                for (Map.Entry<Material, Integer> reversion : newReversions.entrySet()) {
+                    if (reversions.containsKey(reversion.getKey()))
+                        reversions.put(reversion.getKey(), reversions.get(reversion.getKey()) + reversion.getValue());
+                    else
+                        reversions.put(reversion.getKey(), reversion.getValue());
+                }
+            }
+        }
+
+        void give(Material material, int amount, boolean sendMessages) {
+            if (amount > 0)
+                storeUndo(material, -amount);
+
+            while (amount > 0) {
+                int stackSize = Math.min(amount, material.getMaxStackSize());
+                Pair<ItemStack, Integer> slot = nextAddable(material);
+                int index = slot.getSecond();
+
+                if (index == -1) {
+                    player.getWorld().dropItem(player.getLocation(), new ItemStack(material, stackSize));
+                    if (sendMessages) {
+                        player.sendMessage(ChatColor.RED + "Your inventory was full, so you dropped the item.");
+                        sendMessages = false;
+                    }
+                    amount -= stackSize;
+                } else {
+                    ItemStack addTo = slot.getFirst();
+                    if (addTo == null) {
+                        inventory.setItem(index, new ItemStack(material, stackSize));
+                        amount -= stackSize;
+                    } else {
+                        int currentAmount = addTo.getAmount();
+                        int add = Math.min(material.getMaxStackSize() - currentAmount, amount);
+                        addTo.setAmount(currentAmount + add);
+                        amount -= add;
+                    }
+                }
+            }
+        }
+
+        Pair<ItemStack, Integer> nextAddable(Material material) {
+            Integer slot = cachedSlots.get(material);
+            if (slot == null)
+                slot = 0;
+
+            int size = inventory.getSize();
+            ItemStack stack;
+
+            for (int i = 0; i < size; ++i) {
+                int index = (slot + i) % size;
+                stack = inventory.getItem(index);
+                if (stack == null || (stack.getType() == material && stack.getAmount() < material.getMaxStackSize()) ||
+                        stack.getType() == Material.AIR || stack.getAmount() <= 0) {
+                    cachedSlots.put(material, index);
+                    return new Pair<>(stack, index);
+                }
+            }
+
+            cachedSlots.remove(material);
+            return new Pair<>(null, -1);
+        }
+
+        Pair<ItemStack, Integer> nextMatching(Material material) {
+            Integer slot = cachedSlots.get(material);
+            if (slot == null)
+                slot = 0;
+
+            int size = inventory.getSize();
+            ItemStack stack;
+
+            for (int i = 0; i < size; ++i) {
+                int index = (slot + i) % size;
+                stack = inventory.getItem(index);
+                if (stack != null && stack.getType() == material) {
+                    cachedSlots.put(material, index);
+                    return new Pair<>(stack, index);
+                }
+            }
+
+            cachedSlots.remove(material);
+            return new Pair<>(null, -1);
+        }
     }
 }
