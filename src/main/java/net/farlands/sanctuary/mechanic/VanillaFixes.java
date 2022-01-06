b3531906266f3cf41f0cea919d79b248d80c4ba8
kevinbarnes <ksbarnes39@gmail.com>
2022-01-06 17:20:37 -0500

    fix pet teleportation and notify when pets left behind

--- src/main/java/net/farlands/sanctuary/mechanic/VanillaFixes.java
+++ src/main/java/net/farlands/sanctuary/mechanic/VanillaFixes.java
@@ -1,23 +1,39 @@
 package net.farlands.sanctuary.mechanic;
 
+import com.kicas.rp.RegionProtection;
+import com.kicas.rp.data.FlagContainer;
+import com.kicas.rp.data.Region;
+import com.kicas.rp.data.RegionFlag;
+import com.kicas.rp.data.flagdata.EnumFilter;
+import com.kicas.rp.data.flagdata.TrustLevel;
+import com.kicas.rp.data.flagdata.TrustMeta;
 import com.kicas.rp.util.Pair;
 import net.farlands.sanctuary.FarLands;
 import net.farlands.sanctuary.command.player.CommandStack;
+import net.farlands.sanctuary.util.ComponentColor;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.Levelled;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Sittable;
+import org.bukkit.entity.Tameable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockSpreadEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * FarLands custom fixes for Vanilla bugs.
@@ -139,6 +155,93 @@ public class VanillaFixes extends Mechanic {
                     cursor.setAmount(c);
                 }
             }
+        }
+    }
+
+    /**
+     * Notify about pets left behind or teleport them to the player.
+     *
+     * @param event PlayerTeleportEvent
+     */
+    @EventHandler
+    public void onPlayerTeleport(final PlayerTeleportEvent event) {
+        final Player player = event.getPlayer();
+        final Location from = event.getFrom();
+        final Location destination = event.getTo();
+        final Region claim = RegionProtection.getDataManager().getHighestPriorityRegionAt(from);
+
+        // Get all nearby pets from the teleport location
+        final Collection<Tameable> nearbyPets = from.getNearbyLivingEntities(20.0).stream()
+            .filter(
+                animal -> animal instanceof Tameable
+            ).map(
+                animal -> (Tameable) animal
+            ).filter(
+                pet -> pet.getOwnerUniqueId() != null && pet.getOwnerUniqueId().equals(player.getUniqueId())
+            ).toList();
+
+        // Try to teleport standing pets
+        AtomicInteger failedToTeleport = new AtomicInteger();
+        nearbyPets.forEach(pet -> {
+            // Sittable pets (dogs, cats, and parrots) are the only pets that are supposed to tp follow in vanilla
+            if (pet instanceof Sittable sittablePet) {
+                if (!sittablePet.isSitting()) {
+                    if (!teleport(pet, destination.add(1.0, 1.0, 1.0))) {
+                        failedToTeleport.getAndIncrement();
+                    }
+                }
+            }
+        });
+
+        // Notify about pets that failed to teleport due to region flags
+        if (failedToTeleport.get() > 0) {
+            player.sendMessage(
+                ComponentColor.gold("You teleported to a region %s of your pets cannot teleport to! " +
+                        "They were left at ", failedToTeleport.get())
+                    .append(ComponentColor.aqua("%s %s %s", from.getBlockX(), from.getBlockY(), from.getBlockZ()))
+                    .append(ComponentColor.gold("."))
+            );
+        }
+
+        // Check for pets left sitting in the wild or another player's claim
+        if (claim == null || !claim.<TrustMeta>getAndCreateFlagMeta(RegionFlag.TRUST).hasTrust(player, TrustLevel.BUILD, claim)) {
+            AtomicInteger leftBehind = new AtomicInteger();
+            nearbyPets.forEach(pet -> {
+                    if (pet instanceof Sittable sittablePet) {
+                        if (sittablePet.isSitting()) {
+                            leftBehind.getAndIncrement();
+                        }
+                    }
+                });
+
+            // Notify about pets left behind
+            if (leftBehind.get() > 0) {
+                player.sendMessage(
+                    ComponentColor.gold("You left %s of your pets sitting at ", leftBehind.get())
+                        .append(ComponentColor.aqua("%s %s %s", from.getBlockX(), from.getBlockY(), from.getBlockZ()))
+                        .append(ComponentColor.gold("."))
+                );
+            }
+        }
+    }
+
+    /**
+     * Try to teleport an entity. This will return true if the teleport was successful and false if it failed.
+     *
+     * @param entity the entity to teleport
+     * @param destination the location to teleport to
+     * @return success status
+     */
+    private boolean teleport(final @NotNull LivingEntity entity, final @NotNull Location destination) {
+        final FlagContainer flags = RegionProtection.getDataManager().getFlagsAt(destination);
+        if (
+            flags.<EnumFilter.EntityFilter>getFlagMeta(RegionFlag.DENY_ENTITY_TELEPORT).isBlocked(entity.getType()) ||
+                !flags.isAllowed(RegionFlag.FOLLOW)
+        ) {
+            return false;
+        } else {
+            entity.teleport(destination);
+            return true;
         }
     }
 }
