af0b24211ee5c6d0868530ffff766cfe504095b2
funnyboy-roks <funnyboyroks@gmail.com>
2023-06-11 23:24:51 -0500

    Remove backup

--- src/main/resources/scripts/backup.sh
+++ src/main/resources/scripts/backup.sh
@@ -2,7 +2,8 @@
 # Usage: ./backup.sh <screen> <homeDir> <serverDir> <backupDir> <dedicatedMemory>
 
 sleep 12
-screen -S $1 -p 0 -X stuff "cd $2\n"
-screen -S $1 -p 0 -X stuff "time rsync -a --exclude=database.db $3/ $4\n"
-screen -S $1 -p 0 -X stuff "cd $3\n"
+# Temp disabled so we don't backup anarchy
+#screen -S $1 -p 0 -X stuff "cd $2\n"
+#screen -S $1 -p 0 -X stuff "time rsync -a --exclude=database.db $3/ $4\n"
+#screen -S $1 -p 0 -X stuff "cd $3\n"
 screen -S $1 -p 0 -X stuff "./server.sh $5\n"
