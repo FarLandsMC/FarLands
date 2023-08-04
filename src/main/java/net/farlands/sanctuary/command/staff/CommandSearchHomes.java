331ba5e5dda720e284b26fed9434c6839afa56a5
funnyboy-roks <funnyboyroks@gmail.com>
2023-08-04 15:04:40 -0500

    Remove searchome cache because all lookups are < 10ms

--- src/main/java/net/farlands/sanctuary/command/staff/CommandSearchHomes.java
+++ src/main/java/net/farlands/sanctuary/command/staff/CommandSearchHomes.java
@@ -1,6 +1,5 @@
 package net.farlands.sanctuary.command.staff;
 
-import com.kicas.rp.util.Pair;
 import net.farlands.sanctuary.FarLands;
 import net.farlands.sanctuary.chat.Pagination;
 import net.farlands.sanctuary.command.PlayerCommand;
@@ -8,22 +7,13 @@ import net.farlands.sanctuary.data.Rank;
 import net.farlands.sanctuary.util.ComponentColor;
 import net.farlands.sanctuary.util.ComponentUtils;
 import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class CommandSearchHomes extends PlayerCommand {
 
     private static final int MAX_RADIUS = 500;
-
-    // Collection of last searches to be more efficient when searching in pages
-    //        Search Location, Radius : Homes Lines
-    public Map<Pair<Location, Integer>, List<Component>> searchCache = new HashMap<>();
-    private static final int CACHE_TIME = 15; // Minutes
 
     public CommandSearchHomes() {
         super(Rank.JR_BUILDER, "Search for nearby homes.", "/searchhomes <radius>", "searchhomes");
@@ -35,44 +25,37 @@ public class CommandSearchHomes extends PlayerCommand {
             int radius = parseNumber(args[0], Integer::parseInt, -1);
 
             if (radius <= 0 || radius > MAX_RADIUS) {
-                error(sender, "Invalid radius. Must be an integer between 1 and {}.", MAX_RADIUS);
-                return true;
+                return error(sender, "Invalid radius. Must be an integer between 1 and {}.", MAX_RADIUS);
             }
-            List<Component> homes;
-            Pair<Location, Integer> currentSearch = new Pair<>(sender.getLocation(), radius);
-            if (searchCache.containsKey(currentSearch)) {
-                // TODO: Invalidate cache if home created within the distance
-                info(sender, "This does not contain homes created in the last {} minutes.", CACHE_TIME);
-                homes = searchCache.get(currentSearch);
-            } else {
-                homes = FarLands
-                    .getDataHandler()
-                    .getOfflineFLPlayers()
+
+            long start = System.currentTimeMillis();
+            List<Component> homes = FarLands
+                .getDataHandler()
+                .getOfflineFLPlayers()
+                .stream()
+                .flatMap(flp -> flp
+                    .homes
                     .stream()
-                    .flatMap(flp -> flp
-                        .homes
-                        .stream()
-                        .filter(h -> h.getLocation().getWorld().equals(sender.getWorld()))
-                        .filter(h -> h.getLocation().distanceSquared(sender.getLocation()) <= radius * radius)
-                        .map(h -> ComponentColor.gold(flp.username + " - ")
-                            .append(
-                                ComponentUtils.command(
-                                    "/home " + h.getName() + flp.username,
-                                    h.asComponent(false, false),
-                                    ComponentColor.gray("Click to teleport to home.")
-                                )
-                            )
-                        ))
-                    .toList();
-                searchCache.put(currentSearch, homes);
-                Bukkit.getScheduler().runTaskLater(FarLands.getInstance(), () -> searchCache.remove(currentSearch), 20 * 60 * CACHE_TIME);
-            }
+                    .filter(h -> h.getLocation().getWorld().equals(sender.getWorld()))
+                    .filter(h -> h.getLocation().distanceSquared(sender.getLocation()) <= radius * radius)
+                    .map(h -> ComponentColor.gold(
+                             "{} - {}",
+                             flp,
+                             ComponentUtils.command(
+                                 "/home " + h.getName() + flp.username,
+                                 h.asComponent(false, false),
+                                 ComponentColor.gray("Click to teleport to home.")
+                             )
+                         )
+                    ))
+                .toList();
+            FarLands.getDebugger().echo("Found %d homes within %d blocks for %s in %dms".formatted(homes.size(), radius, sender.getName(), System.currentTimeMillis() - start));
 
             if (homes.isEmpty()) {
                 return error(sender, "No homes found within {} blocks.", radius);
             }
 
-            Pagination pagination = new Pagination(ComponentColor.gold("Search Homes"), "/searchomes " + radius);
+            Pagination pagination = new Pagination(ComponentColor.gold("{} homes found", homes.size()), "/searchomes " + radius);
             pagination.addLines(homes);
 
             if (args.length > 1) {
